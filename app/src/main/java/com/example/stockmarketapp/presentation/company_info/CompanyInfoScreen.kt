895f6285c111f29a8907f970ef64535ce1a80c11
KPAHAPK <Roman444.zaharov@gmail.com>
2022-05-12 10:43:39 +0300

    CompanyInfosScreen.kt

--- app/src/main/java/com/example/stockmarketapp/presentation/company_info/CompanyInfoScreen.kt
+++
@@ -1,8 +0,0 @@
-package com.example.stockmarketapp.presentation.company_info
-
-import androidx.compose.runtime.Composable
-import com.ramcosta.composedestinations.annotation.Destination
-
-@Destination
-@Composable
-fun CompanyInfoScreen(){}
